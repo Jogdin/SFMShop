915cd3061d168592cfcf7e5c8b12a8d310c76b0a
jogdin <jogdin@rambler.ru>
2026-04-07 22:48:05 +0700

    Добавлены методы управления складом в класс Product

--- src/models/product.py
+++ src/models/product.py
@@ -1,9 +1,11 @@
 class Product:
-    def __init__(self, name, price, quantity, apply_discount):
+    def __init__(self, name, price, quantity, apply_discount, check_stock, update_stock):
         self.name = name
         self.price = price
         self.quantity = quantity
         self.apply_discount = apply_discount
+        self.check_stock = check_stock
+        self.update_stock = update_stock
         
     def get_total_price(self):
         return self.price * self.quantity
