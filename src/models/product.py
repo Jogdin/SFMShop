4e64833af7e56e63fb0e79edd9278d4620be5da6
jogdin <jogdin@rambler.ru>
2026-04-08 23:24:45 +0700

    Добавлен метод calculate_shipping в Product

--- src/models/product.py
+++ src/models/product.py
@@ -1,11 +1,13 @@
 class Product:
-    def __init__(self, name, price, quantity, apply_discount, check_stock, update_stock):
+    def __init__(self, name, price, quantity, apply_discount, check_stock, update_stock,calculate_shipping):
         self.name = name
         self.price = price
         self.quantity = quantity
         self.apply_discount = apply_discount
         self.check_stock = check_stock
         self.update_stock = update_stock
+        self.calculate_shipping = calculate_shipping
+
         
     def get_total_price(self):
         return self.price * self.quantity
